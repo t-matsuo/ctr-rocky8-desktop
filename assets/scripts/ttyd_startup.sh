68034d5d91bfe59b14d7b1832d0193c636b96642
Takatoshi MATSUO <matsuo.tak@gmail.com>
2024-10-09 00:00:25 +0900

    fix env variable name

--- assets/scripts/ttyd_startup.sh
+++ assets/scripts/ttyd_startup.sh
@@ -4,4 +4,4 @@ mkfifo /var/run/nafder/ttyd
 exec 1>>/var/run/nafder/ttyd
 exec 2>>/var/run/nafder/ttyd
 
-exec /usr/local/bin/ttyd $ENV_TTYD_OPTS -p 57575 -i lo -b /term/ /bin/bash
+exec /usr/local/bin/ttyd $TTYD_OPTS -p 57575 -i lo -b /term/ /bin/bash
