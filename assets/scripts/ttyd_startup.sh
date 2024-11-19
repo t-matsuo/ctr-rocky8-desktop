7e31ed57d93ccbb6fdac5ef5793832cc4d12ca23
MATSUO Takatoshi <matsuo.tak@gmail.com>
2024-11-19 23:53:41 +0900

    update ttyd from 1.6.1.1(original) to 1.7.7

--- assets/scripts/ttyd_startup.sh
+++ assets/scripts/ttyd_startup.sh
@@ -4,4 +4,4 @@ mkfifo /var/run/nafder/ttyd
 exec 1>>/var/run/nafder/ttyd
 exec 2>>/var/run/nafder/ttyd
 
-exec /usr/local/bin/ttyd $TTYD_OPTS -p 57575 -i lo -b /term/ -t 'theme={"background": "black", "foreground": "#b3b3b3"}' /bin/bash
+exec /usr/local/bin/ttyd $TTYD_OPTS -W -p 57575 -i lo -b /term/ -t 'theme={"background": "black", "foreground": "#b3b3b3"}' -- /bin/bash --login
