78390d4192bb706a9549f8d9b9cd0e0616283f46
MATSUO Takatoshi <matsuo.tak@gmail.com>
2025-03-29 20:36:20 +0900

    fix build error and use multi state build to merge layer

--- build.sh
+++ build.sh
@@ -8,47 +8,41 @@ if [ "$TAG" = "" ]; then
     TAG="latest"
 fi
 
-CC_OPTION="-m"
-if [ "$MODE" = "dev" ]; then
-    CC_OPTION="-c"
-    IS_DEV="-dev"
-    sed 's|^RUN|RUN --mount=type=cache,id=rocky8,target=/var/cache/dnf --mount=type=cache,id=rocky8,target=/var/lib/dnf --mount=type=cache,id=rocky8,target=/root/.cache --mount=type=cache,id=rocky8,target=/root/.npm|g' Dockerfile.split > _Dockerfile.split.tmp
-else
-    sed 's|^#__BULDKIT_MARKER__||g' Dockerfile.split > _Dockerfile.split.tmp
-fi
+CC_OPTION="-c"
+sed 's|^RUN|RUN --mount=type=cache,id=rocky8,target=/var/cache/dnf --mount=type=cache,id=rocky8,target=/var/lib/dnf --mount=type=cache,id=rocky8,target=/root/.cache --mount=type=cache,id=rocky8,target=/root/.npm|g' Dockerfile.split > _Dockerfile.split.tmp
 
 if [ "$FLAVOR" = "" ]; then
     FLAVOR="desktop-full"
 fi
 
 if [ "$FLAVOR" = "desktop-min" ]; then
-    DESKTOP="" NGINX="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}${IS_DEV}
+    DESKTOP="" NGINX="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}
 elif [ "$FLAVOR" = "desktop-custom" ]; then
-    DESKTOP="" NGINX="" CODE="" CHROME="" CONTAINER="" AZURE="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}${IS_DEV}
+    DESKTOP="" NGINX="" CODE="" CHROME="" CONTAINER="" AZURE="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}
 elif [ "$FLAVOR" = "desktop-with-filer" ]; then
-    DESKTOP="" NGINX="" FILER="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}${IS_DEV}
+    DESKTOP="" NGINX="" FILER="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}
 elif [ "$FLAVOR" = "desktop-with-term-filer" ]; then
-    DESKTOP="" NGINX="" FILER="" TTYD="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}${IS_DEV}
+    DESKTOP="" NGINX="" FILER="" TTYD="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}
 elif [ "$FLAVOR" = "vscode" ]; then
-    NGINX="" CODE="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}${IS_DEV}
+    NGINX="" CODE="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}
 elif [ "$FLAVOR" = "vscode-custom" ]; then
-    NGINX="" CODE="" CONTAINER="" CHROME="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}${IS_DEV}
+    NGINX="" CODE="" CONTAINER="" CHROME="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}
 elif [ "$FLAVOR" = "term" ]; then
-    NGINX="" TTYD="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}${IS_DEV}
+    NGINX="" TTYD="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}
 elif [ "$FLAVOR" = "term-with-filer" ]; then
-    NGINX="" FILER="" TTYD="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}${IS_DEV}
+    NGINX="" FILER="" TTYD="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}
 elif [ "$FLAVOR" = "term-custom" ]; then
-    NGINX="" TTYD="" CONTAINER="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}${IS_DEV}
+    NGINX="" TTYD="" CONTAINER="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}
 elif [ "$FLAVOR" = "term-with-filer-custom" ]; then
-    NGINX="" FILER="" TTYD="" CONTAINER="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}${IS_DEV}
+    NGINX="" FILER="" TTYD="" CONTAINER="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}
 elif [ "$FLAVOR" = "xrdp" ]; then
-    DESKTOP="" XRDP="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}${IS_DEV}
+    DESKTOP="" XRDP="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}
 elif [ "$FLAVOR" = "xrdp-custom" ]; then
-    DESKTOP="" XRDP="" CONTAINER="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}${IS_DEV}
+    DESKTOP="" XRDP="" CONTAINER="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}
 elif [ "$FLAVOR" = "desktop-full-custom" ]; then
-    DESKTOP="" NGINX="" CODE="" XRDP="" FILER="" SSHD="" TTYD="" CHROME="" CONTAINER="" AZURE="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}${IS_DEV}
+    DESKTOP="" NGINX="" CODE="" XRDP="" FILER="" SSHD="" TTYD="" CHROME="" CONTAINER="" AZURE="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}
 elif [ "$FLAVOR" = "desktop-full" ]; then
-    DESKTOP="" NGINX="" CODE="" XRDP="" FILER="" SSHD="" TTYD="" CHROME="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}${IS_DEV}
+    DESKTOP="" NGINX="" CODE="" XRDP="" FILER="" SSHD="" TTYD="" CHROME="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}
 elif [ "$FLAVOR" = "all" ]; then
     for i in desktop-min desktop-custom desktop-with-filer desktop-with-term-filer vscode vscode-custom term term-custom term-with-filer-custom xrdp xrdp-custom desktop-full-custom desktop-full; do
         echo "------------------- building $i -------------------"
@@ -58,7 +52,7 @@ elif [ "$FLAVOR" = "all" ]; then
     exit 0
 else
     # desktop-full
-    DESKTOP="" NGINX="" CODE="" XRDP="" FILER="" SSHD="" TTYD="" CHROME="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}${IS_DEV}
+    DESKTOP="" NGINX="" CODE="" XRDP="" FILER="" SSHD="" TTYD="" CHROME="" ./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile.${FLAVOR}
 fi
 
 set +e
@@ -79,6 +73,6 @@ else
 fi
 set -e
 
-echo "$BUILDER build -t ${IMAGE_NAME}${FLAVOR}:${TAG}${IS_DEV} -f _Dockerfile.${FLAVOR}${IS_DEV} ."
-DOCKER_BUILDKIT=1 $BUILDER build --progress=plain -t ${IMAGE_NAME}${FLAVOR}:${TAG}${IS_DEV} -f _Dockerfile.${FLAVOR}${IS_DEV} .
-echo "builded from _Dockerfile.$FLAVOR${IS_DEV}"
+echo "$BUILDER build -t ${IMAGE_NAME}${FLAVOR}:${TAG} -f _Dockerfile.${FLAVOR} ."
+DOCKER_BUILDKIT=1 $BUILDER build --progress=plain -t ${IMAGE_NAME}${FLAVOR}:${TAG} -f _Dockerfile.${FLAVOR} .
+echo "builded from _Dockerfile.$FLAVOR"
